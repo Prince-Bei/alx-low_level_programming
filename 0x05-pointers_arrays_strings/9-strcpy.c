743395542cac80ac5c550a044864c4ed8cbde1bb
Imoleayo <imolemanuel@gmail.com>
2022-09-19 13:49:22 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include "2-strlen.c"
 
-/** 
- * _strcpy - main function to copy 
- * 
- * @dest: destination to copy 
- * @src: src 
- * 
- * Return: a character value 
+/**
+ * _strcpy - main function to copy
+ *
+ * @dest: destination to copy
+ * @src: src
+ *
+ * Return: a character value
  */
 
 char *_strcpy(char *dest, char *src)
