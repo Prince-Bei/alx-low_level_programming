bb9532ae883dc82663886a817ab226e1afe7dea1
Imoleayo <imolemanuel@gmail.com>
2022-10-10 15:58:54 -0700

    free dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef dog_h
+#define dog_h
+
 /**
- * struct dog - structure of a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog- creates a struc for a dog
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
  */
 
 struct dog
@@ -13,10 +14,12 @@ char *name;
 float age;
 char *owner;
 };
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
+void free_dog(dog_t *d);
 #endif
