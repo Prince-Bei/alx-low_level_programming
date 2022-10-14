1805bed8ab06b493eb4f5589a7b073653bd82636
Imoleayo <imolemanuel@gmail.com>
2022-10-13 21:25:42 -0700

    func

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef _FUNCTION_VARIADIC_H_
 #define _FUNCTION_VARIADIC_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
